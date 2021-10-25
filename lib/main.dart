cf464ace82c6941404d1184882e29578727ee3b1
FakhriFernanda <fakhrifernanda@gmail.com>
2021-10-26 02:17:56 +0800

    Praktik Widget Card

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new WidgetCard(),
+  ));
+}
+
+class WidgetCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.pink[300],
+      appBar: new AppBar(
+          backgroundColor: Colors.red, title: Text("Portal Ramadhan")),
+      body: Container(
+          child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new CardSaya(
+              icon: Icons.access_alarm_sharp,
+              teks: "Jadwal Imsak",
+              warnaIcon: Colors.pink),
+          new CardSaya(
+              icon: Icons.place,
+              teks: "Masjid Terdekat",
+              warnaIcon: Colors.green),
+          new CardSaya(
+              icon: Icons.access_alarm_rounded,
+              teks: "Jadwal Buka Puasa",
+              warnaIcon: Colors.brown),
+          new CardSaya(
+              icon: Icons.music_note,
+              teks: "Murotal Al-Quran",
+              warnaIcon: Colors.black),
+        ],
+      )),
+    );
+  }
+}
+
+class CardSaya extends StatelessWidget {
+  CardSaya({required this.icon, required this.teks, required this.warnaIcon});
+
+  final IconData icon;
+  final String teks;
+  final Color warnaIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        height: 150,
+        padding: new EdgeInsets.all(3.0),
+        color: Colors.pink,
+        child: new Card(
+          child: new Column(
+            children: <Widget>[
+              new Icon(
+                icon,
+                size: 80.0,
+                color: warnaIcon,
+              ),
+              new Text(
+                teks,
+                style: new TextStyle(fontSize: 23.0),
+              )
+            ],
+          ),
+        ));
+  }
+}
